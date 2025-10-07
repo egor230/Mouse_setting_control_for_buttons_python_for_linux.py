79b50dba5076b4d55d046dc500a827838baced79
egor230 <32921166+egor230@users.noreply.github.com>
2025-10-07 06:18:58 +0500

    Add files via upload

--- Pyqt_libs_mouse.py
+++ Pyqt_libs_mouse.py
@@ -1218,40 +1218,38 @@ class MouseSettingAppMethods:
     list_threads.append(f2)
     f2.start()
     return True
-   try:
-    mouse_listener = mouse.Listener(on_click=on_click)
-    mouse_listener.start()  # Запуск слушателя  # print( game)#  print( dict_save.get_cur_app
-    game = dict_save.get_cur_app()  # какая игра сейчас текущая по вкладке.
-    while not dict_save.thread_exit:  # time.sleep(3)   #print(dict_save.get_flag_thread())
-     new_path_game = check_current_active_window(dict_save, games_checkmark_paths)  # Текущая директория активного окна игры.
-     # Если никакой игры не запущено мы возвращаем предыдущую конфигурацию это директория. #   # print(new_path_game)#
-     if game != new_path_game :  # игра которая сейчас на активной вкладке активного окна    #
-      dict_save.set_cur_app(new_path_game)  # # dict_save.set_current_path_game(new_path_game)
-     if dict_save.get_current_path_game() != dict_save.get_cur_app():  # Если у нас текущий путь к игре отличает от начального    # print(new_path_game)
-      for t in list_threads:
-       t.join()
-       list_threads.remove(t)
-      break#   print("exit")
-    a = key_work.keys_list + key_work.keys_list1
-    # Перебираем все основные кнопки мыши и отпускаем их
-    for button in [Button.left, Button.right, Button.middle]:
-     mouse_controller.release(button)
+   mouse_listener = mouse.Listener(on_click=on_click)
+   mouse_listener.start()  # Запуск слушателя  # print( game)#  print( dict_save.get_cur_app
+   game = dict_save.get_cur_app()  # какая игра сейчас текущая по вкладке.
+   while not dict_save.thread_exit:  # time.sleep(3)   #print(dict_save.get_flag_thread())
+    new_path_game = check_current_active_window(dict_save, games_checkmark_paths)  # Текущая директория активного окна игры.
+    # Если никакой игры не запущено мы возвращаем предыдущую конфигурацию это директория. #   # print(new_path_game)#
+    if game != new_path_game :  # игра которая сейчас на активной вкладке активного окна    #
+     dict_save.set_cur_app(new_path_game)  # # dict_save.set_current_path_game(new_path_game)
+    if dict_save.get_current_path_game() != dict_save.get_cur_app():  # Если у нас текущий путь к игре отличает от начального    # print(new_path_game)
+     for t in list_threads:
+      t.join()
+      list_threads.remove(t)
+     break#   print("exit")
+   a = key_work.keys_list + key_work.keys_list1
+   # Перебираем все основные кнопки мыши и отпускаем их
+   for b in [Button_Controller.left, Button_Controller.right, Button_Controller.middle]:
+    mouse_controller.release(b)
 #    for i in list(key):
-    #   if i in a:  # print(i)
-    #    release = '''#!/bin/bash
-    #      xte 'keyup {0}'    '''
-    #    subprocess.call(['bash', '-c', release.format(key)])
-    mouse_listener.stop()
-    mouse_listener.join()  # Ожидание завершения
-    dict_save.set_thread(0)
-    if dict_save.thread_exit: # Если флаг выхода выходим
-     print("e")
-     return
-    t2 = threading.Thread(target=self.start_startup_now, args=(dict_save,))  # Запустить функцию, которая запускает эмуляцию заново.
-    t2.daemon = True
-    t2.start()  # print("cll")
-   except:
-    pass
+   #   if i in a:  # print(i)
+   #    release = '''#!/bin/bash
+   #      xte 'keyup {0}'    '''
+   #    subprocess.call(['bash', '-c', release.format(key)])
+   mouse_listener.stop()
+   mouse_listener.join()  # Ожидание завершения
+   dict_save.set_thread(0)
+   if dict_save.thread_exit: # Если флаг выхода выходим
+    print("e")
+    return
+   t2 = threading.Thread(target=self.start_startup_now, args=(dict_save,))  # Запустить функцию, которая запускает эмуляцию заново.
+   t2.daemon = True
+   t2.start()  # print("cll")
+
 
   def prepare(self, dict_save, res, games_checkmark_paths):  # функция эмуляций.  # games_checkmark_paths - Список игр с галочкой
    id = res["id"]  # Получаем id устройства  print(id)
